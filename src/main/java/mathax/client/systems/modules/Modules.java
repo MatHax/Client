4e47c8b280e6f44826f9cd46139cfefa0a2884d4
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2022-04-23 09:30:51 +0200

    Auto MLG

--- src/main/java/mathax/client/systems/modules/Modules.java
+++ src/main/java/mathax/client/systems/modules/Modules.java
@@ -434,6 +434,7 @@ public class Modules extends System<Modules> {
         add(new AntiLevitation());
         add(new AntiVoid());
         add(new AutoJump());
+        add(new AutoMLG());
         add(new AutoWalk());
         add(new Blink());
         add(new BoatFly());
