38e3a31795bc89e496c36c86abbe6ac3a5c4a1ef
FurnyGo <68079109+FurnyGo@users.noreply.github.com>
2022-07-10 18:29:01 +0400

    Inventory Tweaks Armor Slots from MeteorClient

---
+++ src/main/java/mathax/client/mixin/PlayerArmorSlotMixin.java
@@ -0,0 +1,28 @@
+package mathax.client.mixin;
+import mathax.client.systems.modules.Modules;
+import mathax.client.systems.modules.misc.InventoryTweaks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net/minecraft/screen/PlayerScreenHandler$1")
+public class PlayerArmorSlotMixin {
+
+    @Inject(method = "getMaxItemCount", at = @At("HEAD"), cancellable = true)
+    private void onGetMaxItemCount(CallbackInfoReturnable<Integer> cir) {
+        if (Modules.get().get(InventoryTweaks.class).armorStorage()) cir.setReturnValue(64);
+    }
+
+    @Inject(method = "canInsert", at = @At("HEAD"), cancellable = true)
+    private void onCanInsert(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (Modules.get().get(InventoryTweaks.class).armorStorage()) cir.setReturnValue(true);
+    }
+
+    @Inject(method = "canTakeItems", at = @At("HEAD"), cancellable = true)
+    private void onCanTakeItems(PlayerEntity playerEntity, CallbackInfoReturnable<Boolean> cir) {
+        if (Modules.get().get(InventoryTweaks.class).armorStorage()) cir.setReturnValue(true);
+    }
+}
